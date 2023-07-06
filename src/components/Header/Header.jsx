f24caadbd8a9f21c457f4d8075647c66bdec5b9b
Mikhail Chekov <chekoff@list.ru>
2023-07-06 17:54:15 +0300

    feat: add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logoIcon from'../../img/icons/logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className="container">
+        <div className="header__row">
+          <div className="header__logo">
+            <img src={logoIcon} alt="logo" />
+            <span>Fashion</span>
+          </div>
+          <nav className="header__nav">
+            <ul>
+              <li><a href="#!">CATALOGUE</a></li>
+              <li><a href="#!">FASHION</a></li>
+              <li><a href="#!">FAVOURITE</a></li>
+              <li><a href="#!">LIFESTYLE</a></li>
+              <li><a href="#!" className="header__nav-btn">SIGN UP</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>  
+  )
+}
+
+export default Header;
+
