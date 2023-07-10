58c95156b77dbca3bcedb9f700aeda4b4d8a1563
Mikhail Chekov <chekoff@list.ru>
2023-07-10 15:18:24 +0300

    feat: finish section Arrivals and Card component

--- src/components/Arrivals/Arrivals.jsx
+++ src/components/Arrivals/Arrivals.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Card from '../Card/Card';
 import './Arrivals.css';
+import cat01Img from "../../img/categories/cat-01.jpg";
+import cat02Img from '../../img/categories/cat-02.jpg';
+import cat03Img from '../../img/categories/cat-03.jpg';
 
 function Arrivals() {
   return (
@@ -10,11 +13,9 @@ function Arrivals() {
                 <h2 className="arrivals__title title-2">NEW ARRIVALS</h2>
             </div>
             <div className="arrivals__cards">
-                <Card />
-                <Card />
-                <Card />
-                {/* <Card />
-                <Card /> */}
+                <Card title="Hoodies & Sweetshirt" img={cat01Img} />
+                <Card title="Coats & Parkas" img={cat02Img}/>
+                <Card title="Tees & T-Shirt" img={cat03Img}/>
             </div>
         </div>
     </section>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import styles from "./Card.module.css";
-import cat01Img from "../../img/categories/cat-01.jpg";
 import cardIconArrow from "../../img/icons/arrow.svg";
-// import cat02Img from '../../img/categories/cat-02.jpg';
-// import cat03Img from '../../img/categories/cat-03.jpg';
 
-function Card() {
+function Card({ title, img }) {
   return (
     <div className={styles.card}>
-      <a href="#!" className={styles.card__link}><span className={styles.none}>!</span></a>
+      <a href="#!" className={styles.card__link}>
+        <span className={styles.none}>!</span>
+      </a>
       <div className={styles.card__imgwrap}>
-        <img src={cat01Img} alt="cat01" className={styles.card__img} />
+        <img src={img} alt="cat01" className={styles.card__img} />
       </div>
       <div className={styles.card__body}>
         <div className={styles.card__text}>
-          <div className={styles.card__title}>Hoodies & Sweetshirt</div>
+          <div className={styles.card__title}>{title}</div>
           <div className={styles.card__muted}>Explore Now!</div>
         </div>
         <div className={styles.card__icon}>
