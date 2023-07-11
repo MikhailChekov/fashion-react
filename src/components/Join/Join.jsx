29468bba3c739909e3a739b56c58a187eea073b6
Mikhail Chekov <chekoff@list.ru>
2023-07-11 19:40:12 +0300

    feat: finish Join section

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -9,13 +9,13 @@ function Join() {
           <div className="join__title">
             JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO
           </div>
-          <div className="join_desc">
+          <div className="join__desc">
             Type your email down below and be young wild generation
           </div>
-          <div className="join__form">
-            <form>
-              <input type="text" placeholder="Add your email here" />
-              <button type="submit">Send</button>
+          <div className="join__form-container">
+            <form className="join__form">
+              <input type="email"  className="join__form-input" placeholder="Add your email here" />
+              <button className="join__form-btn" type="submit">Send</button>
             </form>
           </div>
         </div>
